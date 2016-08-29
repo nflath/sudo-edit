4af2f3299a9a648235fa6c90fee6c59761893f5d
Mario Rodas <marsam@users.noreply.github.com>
2016-08-29 17:17:48 -0500

    Add tramp support
    + Allow to edit as another user not only root
    + Obsolete `sudo-edit-current-file`

--- sudo-edit.el
+++ sudo-edit.el
@@ -41,22 +41,60 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Code:
+(eval-when-compile
+  (require 'cl-lib)
+  (require 'subr-x))
+
+(require 'tramp)
+
+(defgroup sudo-edit nil
+  "Edit files as another user."
+  :prefix "sudo-edit-"
+  :group 'convenience)
+
+(defcustom sudo-edit-user "root"
+  "Default user to edit a file with `sudo-edit'."
+  :type 'string
+  :group 'sudo-edit)
+
+(defvar sudo-edit-user-history nil)
+
+(defun sudo-edit-filename (filename user)
+  "Return a tramp edit name for a FILENAME as USER."
+  (if (file-remote-p filename)
+      (let* ((vec (tramp-dissect-file-name filename))
+             (hop (tramp-make-tramp-file-name
+                   (tramp-file-name-method vec)
+                   (tramp-file-name-user vec)
+                   (tramp-file-name-host vec)
+                   ""
+                   (tramp-file-name-hop vec))))
+        (setq hop (string-remove-prefix tramp-prefix-format hop))
+        (setq hop (string-remove-suffix tramp-postfix-host-format hop))
+        (setq hop (concat hop tramp-postfix-hop-format))
+        (tramp-make-tramp-file-name "sudo" user (tramp-file-name-host vec) (tramp-file-name-localname vec) hop))
+    (tramp-make-tramp-file-name "sudo" user "localhost" (expand-file-name filename))))
 
 ;;;###autoload
 (defun sudo-edit (&optional arg)
-  "Find a file and open it as root."
-  (interactive "p")
-  (if arg
-      (find-file (concat "/sudo:root@localhost:" (read-file-name "File: ")))
-    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+  "Edit currently visited file as another user, by default `sudo-edit-user'.
 
-;;;###autoload
-(defun sudo-edit-current-file ()
-  "Edit the current file as root."
-  (interactive)
-  (let ((pos (point)))
-    (find-alternate-file (concat "/sudo:root@localhost:" (buffer-file-name (current-buffer))))
-    (goto-char pos)))
+With a prefix ARG prompt for a file to visit.  Will also prompt
+for a file to visit if current buffer is not visiting a file."
+  (interactive "P")
+  (let ((user (if arg
+                  (completing-read "User: " (system-users) nil nil nil 'sudo-edit-user-history sudo-edit-user)
+                sudo-edit-user))
+        (filename (or buffer-file-name
+                      (and (derived-mode-p 'dired-mode) default-directory))))
+    (cl-assert (not (string-blank-p user)) nil "User must not be a empty string")
+    (if (or arg (not filename))
+        (find-file (sudo-edit-filename (read-file-name (format "Find file (as \"%s\"): " user)) user))
+      (let ((position (point)))
+        (find-alternate-file (sudo-edit-filename filename user))
+        (goto-char position)))))
+
+(define-obsolete-function-alias 'sudo-edit-current-file 'sudo-edit)
 
 (provide 'sudo-edit)
 ;;; sudo-edit.el ends here
